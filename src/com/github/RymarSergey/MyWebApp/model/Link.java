66389672b20781c8740c92b725156c84f0789d94
Sergey Rymar <sergey.rymar.ne@yandex.ru>
2018-05-01 15:50:33 +0300

    class Link overide equals and hash

--- src/com/github/RymarSergey/MyWebApp/model/Link.java
+++ src/com/github/RymarSergey/MyWebApp/model/Link.java
@@ -4,6 +4,8 @@ package com.github.RymarSergey.MyWebApp.model;
  *autor Сергей on 01.05.2018 13:22
  */
 
+import java.util.Objects;
+
 public class Link {
     private String name;
     private String url;
@@ -11,5 +13,24 @@ public class Link {
     public Link(String name, String url) {
         this.name = name;
         this.url = url;
+    }
+    public Link(Link link){
+        this.name=link.name;
+        this.url=link.url;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Link link = (Link) o;
+        return Objects.equals(name, link.name) &&
+                Objects.equals(url, link.url);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(name, url);
     }
 }
--- src/lesson01/Main.java
+++ src/lesson01/Main.java
@@ -1,7 +1,15 @@
 package lesson01;
 
+import com.github.RymarSergey.MyWebApp.model.Link;
+
 public class Main {
     public static void main(String[] args) {
+        Link l1=new Link("JavaWebinar","javawebinar.ru");
 
+        Link l2=l1;
+        System.out.println(l1.equals(l2));
+        Link l3=new Link(l1);
+        System.out.println(l1.equals(l3));
+        System.out.println(l1.getClass());
     }
 }
