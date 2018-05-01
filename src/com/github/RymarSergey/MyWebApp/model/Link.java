3a974539b987f4ce71eedfbd487d093e5cb44e95
Sergey Rymar <sergey.rymar.ne@yandex.ru>
2018-05-01 13:26:26 +0300

    add class Link

---
+++ src/com/github/RymarSergey/MyWebApp/model/Link.java
@@ -0,0 +1,9 @@
+package com.github.RymarSergey.MyWebApp.model;/*
+ *
+ *autor Сергей on 01.05.2018 13:22
+ */
+
+public class Link {
+    private String name;
+    private String url;
+}
---
+++ src/com/github/RymarSergey/MyWebApp/model/Resume.java
@@ -0,0 +1,9 @@
+package com.github.RymarSergey.MyWebApp.model;
+
+
+public class Resume {
+    private String fullname;
+    private String location;
+
+
+}
