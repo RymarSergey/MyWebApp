12270c2d21aa844207bfe19fa0314a871008c1d1
Sergey Rymar <sergey.rymar.ne@yandex.ru>
2018-05-01 16:44:10 +0300

    Add constructor and getters in class Contact

--- src/com/github/RymarSergey/MyWebApp/model/Contact.java
+++ src/com/github/RymarSergey/MyWebApp/model/Contact.java
@@ -1,7 +1,23 @@
-package com.github.RymarSergey.MyWebApp.model;/*
+package com.github.RymarSergey.MyWebApp.model;
+/*
  *
  *autor Сергей on 01.05.2018 16:33
  */
 
 public class Contact {
+    private final String type;
+    private final String value;
+
+    public Contact(String type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
--- src/com/github/RymarSergey/MyWebApp/model/Resume.java
+++ src/com/github/RymarSergey/MyWebApp/model/Resume.java
@@ -1,9 +1,11 @@
 package com.github.RymarSergey.MyWebApp.model;
 
 
+import java.util.List;
+
 public class Resume {
     private String fullname;
     private String location;
-
+    private List<Contact> contacts;
 
 }
