f78121ab07c893dfbdbc63a8f52e7a688a45423b
Sergey Rymar <sergey.rymar.ne@yandex.ru>
2018-05-02 16:00:26 +0300

    add ArrayStorageTest
    Signed-off-by: Sergey Rymar <sergey.rymar.ne@yandex.ru>

---
+++ test/com/github/RymarSergey/MyWebApp/storage/ArrayStorageTest.java
@@ -0,0 +1,38 @@
+package com.github.RymarSergey.MyWebApp.storage;
+
+import org.junit.jupiter.api.Test;
+
+/*
+ *
+ *autor Сергей on 02.05.2018 15:48
+ */
+class ArrayStorageTest {
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void load() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAllSorted() {
+    }
+
+    @Test
+    void size() {
+    }
+}
